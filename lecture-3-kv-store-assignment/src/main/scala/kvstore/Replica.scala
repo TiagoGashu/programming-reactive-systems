cf86b5f6d712af5ea5052bb6c5fb7c4a5c8c2400
TiagoGashu <tiago.gashu@gmail.com>
2019-06-10 23:14:30 -0300

    lecture 3 - kv-store - step 5 and partial step 6

--- lecture-3-kv-store-assignment/src/main/scala/kvstore/Replica.scala
+++ lecture-3-kv-store-assignment/src/main/scala/kvstore/Replica.scala
@@ -50,9 +50,18 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
 
   // keeps the requesters mapped by id
   var requesters = Map.empty[Long, ActorRef]
+
+  // maps from id to tuple (first value is primary and second is all replicas)
+  var completeness = Map.empty[Long, (Boolean, Boolean)]
+
   // maps from seq to sender
   var requestersSnaps = Map.empty[Long, (ActorRef, Persist)]
-  var pendingPersists = Map.empty[Long, Cancellable]
+
+  var pendingPersists = Map.empty[Long, (Cancellable, Cancellable, Map[ActorRef, Cancellable])]
+
+  // maps from id to set of replicators
+  var pendingReplicators = Map.empty[Long, Set[ActorRef]]
+
   var expectedSeq: Long = 0L
 
   def getLastSeq() = expectedSeq - 1
@@ -71,25 +80,92 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
       val deletedReplicas = secondaries.keySet diff secondaryReplicas
       newReplicas foreach createReplicator
       deletedReplicas foreach deleteReplicator
+      println(s"I have ${replicators.size} replicators")
+
     case Insert(k, v, id) => update(k, Some(v), id)
     case Remove(k, id) => update(k, None, id)
     case Get(k, id) =>
       val value = if(kv contains k) Some(kv(k)) else None
       sender ! GetResult(k, value, id)
+
     case Persisted(_, id) =>
-      val requester = requesters(id)
-      requesters = requesters - id
-      requester ! OperationAck(id)
+      updatePrimaryCompleteness(id)
+
+      val (primaryCancellable, cancellableTimeLimit, cancellableReplicators) = pendingPersists(id)
+      primaryCancellable.cancel()
+      if(completed(id)) {
+        cancellableTimeLimit.cancel()
+        cancellableReplicators foreach { case (_, v) => v.cancel() }
+        val requester = requesters(id)
+        requesters = requesters - id
+        pendingReplicators -= id
+        requester ! OperationAck(id)
+      }
+
+    case Replicated(_, id) =>
+      // TODO: control replication of new/deleted replicas
+
+      val replicator = sender
+      val updatedReplicators = pendingReplicators(id) - replicator
+      if (updatedReplicators.isEmpty) {
+        updateSecondariesCompleteness(id)
+      }
+
+      val (cancellable, cancellableTimeLimit, cancellableReplicators) = pendingPersists(id)
+      pendingReplicators += (id -> (pendingReplicators(id) - replicator))
+      if (completed(id)) {
+        cancellable.cancel()
+        cancellableTimeLimit.cancel()
+        cancellableReplicators foreach { case (_, v) =>
+          v.cancel()
+        }
+        val requester = requesters(id)
+        requesters = requesters - id
+        pendingReplicators -= id
+        requester ! OperationAck(id)
+      }
   }
 
   private def update(k: String, v: Option[String], id: Long) = {
-    requesters += (id -> sender)
+    val requester = sender
+
+    requesters += (id -> requester)
     if(v.isEmpty) kv -= k
     else kv += (k -> v.get)
-    persister ! Persist(k, v, id)
-    replicators foreach { _ ! Replicate(k, v, id) }
+    val persist = Persist(k, v, id)
+
+    // primary persistence
+    val cancellable = context.system.scheduler.schedule(0 milliseconds, 100 milliseconds)(persister ! persist)
+
+    pendingReplicators += (id -> replicators)
+    var mapOfCancellableReplicators = Map.empty[ActorRef, Cancellable]
+    replicators foreach { r =>
+      mapOfCancellableReplicators += (r -> context.system.scheduler.schedule(0 millisecond, 100 milliseconds)(r ! Replicate(k, v, id)))
+    }
+
+    val cancellableTimeLimit = context.system.scheduler.scheduleOnce(1 second) {
+      cancellable.cancel()
+      mapOfCancellableReplicators foreach { case(_, v) => v.cancel() }
+      requester ! OperationFailed(id)
+    }
+
+    completeness += (id -> Tuple2(false, replicators.isEmpty))
+    pendingPersists += (id -> Tuple3(cancellable, cancellableTimeLimit, mapOfCancellableReplicators))
   }
 
+  private def updatePrimaryCompleteness(id: Long) = {
+    val (_, secondaries) = completeness(id)
+    completeness += (id -> Tuple2(true, secondaries))
+  }
+
+  private def updateSecondariesCompleteness(id: Long) = {
+    val (primary, _) = completeness(id)
+    completeness += (id -> Tuple2(primary, true))
+  }
+
+  private def completed(id: Long) = completeness(id)._1 && completeness(id)._2
+
+  // TODO:
   private def createReplicator(replica: ActorRef) = {
     val newReplicator = context.actorOf(Replicator.props(replica))
     secondaries += (replica -> newReplicator)
@@ -97,10 +173,12 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
     kv foreach { case (k, v) => newReplicator ! Replicate(k, Some(v), -1) }
   }
 
+  // TODO:
   private def deleteReplicator(delReplica: ActorRef) = {
     val deletedReplicator = secondaries(delReplica)
     secondaries -= delReplica
     replicators -= deletedReplicator
+    context.stop(deletedReplicator)
   }
 
   val replica: Receive = {
@@ -120,13 +198,12 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
         if (v.isEmpty) kv -= k
         else kv += (k -> v.get)
         val persist = Persist(k, v, seq)
-        val tuple = (sender, persist)
-        requestersSnaps += (seq -> tuple)
+        requestersSnaps += (seq -> Tuple2(sender, persist))
         val cancellable = context.system.scheduler.schedule(0 seconds, 100 milliseconds)(persister ! persist)
-        pendingPersists += (seq -> cancellable)
+        pendingPersists += (seq -> Tuple3(cancellable, null, null))
       }
     case Persisted(k, seq) =>
-      val cancellable = pendingPersists(seq)
+      val (cancellable, _, _) = pendingPersists(seq)
       cancellable.cancel()
       val (requester, _) = requestersSnaps(seq)
       requestersSnaps -= seq
